7a3a74ec582c5954daf9e1924079efd36631597b
Alexandre <averville1@gmail.com>
2018-04-08 13:18:32 -0400

    Added and made a few changes to Game.h & Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -4,41 +4,116 @@
 #include "Game.h"
 using namespace std;
 
-/////////////////////////Player CLASS/////////////////////////
-//Constructors
-Player::Player() {
 
-}
-//Getters
-//Setters
-
+//
 /////////////////////////Square CLASS/////////////////////////
+
 //Constructors
 Square::Square() {
+    posX = 0;
+    posY = 0;
+    rotationOfSquare=rotate0;
 
+}
+Square::Square(int newPosX, int newPosY) {
+    posX = newPosX;
+    posY = newPosY;
+    rotationOfSquare=rotate0;
+
+}
+Square::Square(int newPosX, int newPosY, squareRotate newRotation) {
+    posX = newPosX;
+    posY = newPosY;
+    rotationOfSquare=newRotation;
 
 }
 //Getters
-//Setters
+int     Square::getPosX() const {
+    return posX;
 
+}
+int     Square::getPosY()  const {
+    return posY;
+}
+squareRotate getRotationOfSquare() {
+    return rotate0;
+}
+//Setters
+void Square::setPosX(int newPosX){
+    posX=newPosX;
+}
+void Square::setPosY(int newPosY){
+    posX=newPosY;
+}
+squareRotate setRotationOfSquare(squareRotate newRotation) {
+    return newRotation;
+}
 /////////////////////////Piece CLASS/////////////////////////
 //Constructors
 Piece::Piece() {
-
-
-
+    colorOfPiece = unknownColor;
+    length = 0.0;
+    height = 0.0;
+}
+Piece::Piece(pieceColors colorOfPiece) {
+    colorOfPiece = colorOfPiece;
+    length = 0.0;
+    height = 0.0;
+}
+Piece::Piece(double newLength, double newHeight) {
+    colorOfPiece = unknownColor;
+    length = newLength;
+    height = newHeight;
 }
 //Getters
+pieceColors     Piece::getPieceColor() const{
+    return colorOfPiece;
+}
+double          Piece::getLength()     const{
+    return length;
+}
+double          Piece::getHeight()     const{
+    return height;
+}
 //Setters
+void        Piece::setPieceColor(pieceColors newPieceColor){
+    colorOfPiece= newPieceColor;
+}
+void        Piece::setLength(double newLength){
+    length=newLength;
+}
+void        Piece::setHeight(double newHeight){
+    height=newHeight;
+}
 
 /////////////////////////Board CLASS/////////////////////////
 //Constructors
 Board::Board() {
-
-
+    boardSizeX=20;
+    boardSizeY=20;
 }
+
+Board::Board(int newBoardSizeX, int newBoardSizeY){
+    int boardSizeX=newBoardSizeX;
+    int boardSizeY=newBoardSizeY;
+}
+
 //Getters
+int Board::getBoardSizeX()  const {
+    return boardSizeX;
+}
+int Board::getBoardSizeY()  const{
+    return boardSizeY;
+}
+
 //Setters
+void Board::setBoardSizeX(int newBoardSizeX){
+    boardSizeX=newBoardSizeX;
+}
+void Board::setBoardSizeY(int newBoardSizeY){
+    boardSizeY= newBoardSizeY;
+}
+
 
 /////////////////////////Blokus CLASS/////////////////////////
 //Constructors
@@ -54,4 +129,37 @@ Blokus::Blokus() {
 Computer::Computer() {
 
 
+}
+
+/////////////////////////Player CLASS/////////////////////////
+//Constructors
+Player::Player() {
+   playerScore=0;
+   pieceColor=unknownColor;
+}
+Player::Player(pieceColors newPieceColor){
+    playerScore=0;
+    pieceColor=newPieceColor;
+
+}
+Player::Player(pieceColors newPieceColor, int newPlayerScore){
+    playerScore=newPlayerScore;
+    pieceColor=newPieceColor;
+
+}
+
+//Getters
+int Player::getPlayerScore() const{
+    return playerScore;
+}
+pieceColors Player::getPieceColor() const{
+    return pieceColor;
+}
+
+//Setters
+void Player::setPlayerScore(int newScore) {
+    playerScore=newScore;
+}
+void Player::setPieceColor(pieceColors newPieceColor){
+    pieceColor=newPieceColor;
 }
--- Game.h
+++ Game.h
@@ -8,50 +8,78 @@
 #include <string>
 // enum for color of pieces
 enum pieceColors {red, blue, yellow, green, unknownColor};
+enum squareRotate {rotate0, rotate90, rotate180, rotate270};
 
-class Player { //unrelated class
-private:
-pieceColors colorOfPiece=unknownColor;
-
-public:
-//Constructors
-Player();
-Player(pieceColors newPieceColors);
-
-
-};
-
-
+//abstract class
 class Square {
 private:
-
+int          posX = 0;
+int          posY = 0;
+squareRotate rotationOfSquare=rotate0;
 
 public:
 //Constructors
 Square();
+Square(int newPosX, int newPosY);
+Square(int newPosX, int newPosY, squareRotate newRotation);
 
+//Getters
+int          getPosX()             const ;
+int          getPosY()             const ;
+squareRotate getRotationOfSquare() const ;
+
+
+//Setters
+void setPosX(int newPosX);
+void setPosY(int newPosY);
+void setRotationOfSquare(squareRotate newRotation);
 };
 
 
 class Piece: public Square{   //pieces are made of squares so, it will inherent from square
 private:
+    pieceColors colorOfPiece = unknownColor;
+    double length = 0.0;
+    double height = 0.0;
 
 
 public:
 //Constructors
 Piece();
+Piece(pieceColors newPieceColor);
+Piece(double newLength, double newHeight);
 
+
+//Getters
+pieceColors     getPieceColor() const;
+double          getLength()     const;
+double          getHeight()     const;
+
+//Setters
+void        setPieceColor(pieceColors newPieceColor);
+void        setLength(double newLength);
+void        setHeight(double newHeight);
 };
 
 class Board: public Square{
 private:
-
+    int boardSizeX=20;
+    int boardSizeY=20;
 
 public:
+
+
 //Constructors
 Board();
+Board(int newBoardSizeX, int newBoardSizeY);
 
+//Getters
+int getBoardSizeX()  const;
+int getBoardSizeY()  const;
 
+//Setters
+void setBoardSizeX(int newBoardSizeX);
+void setBoardSizeY(int newBoardSizeY);
 };
 
 
@@ -59,10 +87,14 @@ class Blokus: public Board {
 private:
 
 
-
 public:
 //Constructors
     Blokus();
+
+//Getters
+
+
+//Setters
 };
 
 class Computer{
@@ -72,9 +104,35 @@ private:
 public:
 //Constructors
     Computer();
+//Getters
+
+
+
+//Setters
 
 };
 
+class Player { //unrelated class will interact with the game pieces and game board
+private:
+    int playerScore=0;
+    pieceColors pieceColor=unknownColor;
+
+public:
+//Constructors
+    Player();
+    Player(pieceColors newPieceColor);
+    Player(pieceColors newPieceColor, int newPlayerScore);
+
+
+//Getters
+    int getPlayerScore() const;
+    pieceColors getPieceColor() const;
+
+
+//Setters
+    void setPlayerScore(int newScore);
+    void setPieceColor(pieceColors newPieceColor);
+};
 
 
 
